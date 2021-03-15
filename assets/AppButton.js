55cce2c7add245d5428059fd2196b9a34b90e73d
julio <empjulio@hotmail.com>
2021-03-15 10:28:36 -0400

    'button'

--- assets/AppButton.js
+++ assets/AppButton.js
@@ -13,7 +13,7 @@ const AppButton = ({ onPress, title }) =>
 const styles = StyleSheet.create({
   appButtonContainer: {
     elevation: 8,
-    backgroundImage: "linear-gradient(#02AFA1, #02675F)",
+    backgroundColor: '#02AFA1',
     borderRadius: 30,
     paddingVertical: 10,
     paddingHorizontal: 12,
